aee2d07b1bd615dfb4bff6e8b4cd1df89175de08
Alexandre Stanescot <alex@aerostan.com>
2022-10-19 19:19:11 +0200

    Chapter finish

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -4,7 +4,6 @@
 function describeCountry(country, population, capitalCity) {
   return `${country} has ${population} million people and its capital is ${capitalCity}`
 }
-
 const country1 = describeCountry("Finland", 6, "Helsinki");
 const country2 = describeCountry("France", 66, "Paris");
 const country3 = describeCountry("Germany", 80, "Berlin");
@@ -12,7 +11,6 @@ console.log(country1);
 console.log(country2);
 console.log(country3);
 */
-
 
 // Lecture functions declarations vs. expressions
 function percentageOfWorld1(population) {
@@ -22,9 +20,9 @@ const franceRes = percentageOfWorld1(66);
 const germanyRes = percentageOfWorld1(80);
 const finlandRes = percentageOfWorld1(8);
 
-console.log(franceRes);
-console.log(germanyRes);
-console.log(finlandRes);
+//console.log(franceRes);
+//console.log(germanyRes);
+//console.log(finlandRes);
 
 const percentageOfWorld2 = function(population) {
   return `${(population / 7900) * 100}%`;
@@ -34,20 +32,72 @@ const franceRes2 = percentageOfWorld2(66);
 const germanyRes2 = percentageOfWorld2(80);
 const finlandRes2 = percentageOfWorld2(8);
 
-console.log(franceRes2);
-console.log(germanyRes2);
-console.log(finlandRes2);
+//console.log(franceRes2);
+//console.log(germanyRes2);
+//console.log(finlandRes2);
 
 
 // Lecture Arrow functions
 
 const percentageOfWorld3 = (population) => `${(population / 7900) * 100}%`;
 const franceRes3 = percentageOfWorld3(66);
-console.log(franceRes3);
+//console.log(franceRes3);
 
 // Functions calling other functions
 
 function describePopulation(country, population) {
   return `${country} has ${population} million people, which is about ${percentageOfWorld1(population)} of the world`;
 }
-console.log(describePopulation("china", "1441"));
+//console.log(describePopulation("china", "1441"));
+
+
+// Lecture Introduction to arrays
+const populations = [66, 80, 32, 8];
+//console.log(populations.length === 4);
+const percentages = [ percentageOfWorld1(populations[0]),percentageOfWorld1(populations[1]),percentageOfWorld1(populations[2]),percentageOfWorld1(populations[3]) ];
+
+// Lecture BasicBasic Array Operations
+const neighbours = ['germany', "italy", "spain"];
+neighbours.push('utopia');
+//console.log(neighbours);
+neighbours.pop();
+//console.log(neighbours);
+if(!neighbours.includes("germany")) {
+  console.log("Probably not a central European country :D'");
+}
+const index = neighbours.indexOf('italy');
+neighbours[index] = "belgium";
+//console.log(neighbours);
+
+// Lecture Introduction to objects
+//const myCountry = {
+//  country: "France",
+//  capital: "Paris",
+//  language: "french",
+//  population: 66,
+//  neighbours: 3
+//}
+
+// Lecture Dot vs. Brackets Notation
+//console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours} neighbouring countries and a capital called ${myCountry.capital}`);
+
+// Object Methods
+const myCountry = {
+  country: "France",
+  capital: "Paris",
+  language: "french",
+  population: 66,
+  neighbours: 3,
+
+  describe: function() {
+    //console.log(`${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours} neighbouring countries and a capital called ${this.capital}`);
+  },
+
+ checkIsland: function() {
+    this.isIsland = this.neighbours === 0 ? true : false
+ }
+};
+
+//console.log(myCountry.describe());
+//console.log(myCountry.checkIsland());
+
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,114 @@
-//'use strict'
+'use strict'
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriverLicense = true;
 if (hasDriversLicense) console.log("I can drive");
+*/
+
+// Coding challenge #1
+//TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+//TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+/*
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const avgDolphins = calcAverage(85, 54, 41);
+const avgKoalas = calcAverage(23, 34, 27);
+//console.log(checkWinner(avgDolphins, avgKoalas));
+
+function checkWinner(averageDolphins, averageKoalas) {
+  let msg;
+  const winDolphins = averageDolphins >= 2 * averageKoalas;
+  const winKoalas   = averageKoalas >= 2 * averageDolphins;
+  
+  if(winDolphins) {
+    msg = `Dolphins wins by a score of ${averageDolphins} - ${averageKoalas} !`
+  } else if (winKoalas) {
+    msg = `Koalas wins by a score of ${averageKoalas} - ${averageDolphins} !`
+  } else {
+    msg = "No winner!";
+  }
+
+  return msg;
+}
+*/
+
+// Coding challenge #2
+/*
+const bill = 125;
+const calcTip = (bill) => bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+console.log(calcTip(100));
+const bills = [ 125, 555, 44 ];
+const tips  = [ calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]) ];
+console.log(tips);
+const total = [ tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2] ]
+console.log(total);
+*/
+
+// Coding Challenge
+/*
+const jonas = {
+  firstName: "Jonas",
+  friends:  [ "Bob", "Bill", "Michael" ]
+}
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, but his best friend is called ${jonas.friends[2]}`);
+*/
+
+// Coding challenge #3
+
+const mark = {
+  firstName: "Mark",
+  lastName: "Miller",
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height ** 2);
+    return this.bmi;
+  }
+};
+
+const john = {
+  firstName: "John",
+  lastName: "Smith",
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height ** 2);
+    return this.bmi;
+  }
+};
+
+mark.calcBMI();
+john.calcBMI();
+if(mark.bmi > john.bmi) {
+  //console.log(`${mark.firstName} ${mark.lastName}'s BMI (${mark.bmi}) is higher than ${john.firstName} ${john.lastName}'s (${john.bmi})`)
+} else {
+  //console.log(`${john.firstName} ${john.lastName}'s BMI (${john.bmi}) is higher than ${mark.firstName} ${mark.lastName}'s (${john.bmi})`)
+}
+console.log(mark, john);
+
+// Coding challenge #4
+const bills   = [ 22, 295, 176, 440, 37, 105, 10, 1100, 86, 52 ];
+const tips    = [];
+const totals  = [];
+const calcTip = bills => bills >= 50 && bills <= 300 ? 0.15 * bills : 0.2 * bills;
+const arr     = [];
+let sum       = 0;
+for(let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(tips[i] + bills[i]);
+}
+console.log(bills, tips, totals);
+
+function calcAverage(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+console.log(calcAverage(totals));
+
