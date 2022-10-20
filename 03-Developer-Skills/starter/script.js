aba9567e20618d02fb8fddf6044b2a9606a567e2
Alexandre Stanescot <alex@aerostan.com>
2022-10-20 16:49:23 +0200

    Excerice ongoing

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,24 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// Coding challenge #1
+const arr = [17, 21, 23];
+
+function printForecast(arr) {
+  // Variables init
+  let result = "...";
+
+  // We iterate in the array
+  for (let i = 0; i < arr.length; i++) {
+    // We store the temperature
+    let temp = arr[i];
+
+    // We write the string for current temperature
+    const currentData = `${temp}°C in ${i + 1} days ...`;
+    // We concat the result into a main string
+    result += currentData;
+  }
+  console.log(result);
+}
+
+console.log(printForecast(arr));
