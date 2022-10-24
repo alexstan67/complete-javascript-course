61bcd8eb203d8b2f8cf8cd8f81b9b54c6ef8bfb0
Alexandre Stanescot <alex@aerostan.com>
2022-10-24 18:51:20 +0200

    Challenge finished

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,101 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+//const [x, y, z] = arr;
+//console.log(arr);
+//console.log(x, y, z);
+// --------------------
+const [main, , secondary] = restaurant.categories;
+//console.log(main, secondary);
+// Switching variables
+//[main, secondary] = [secondary, main];
+// --------------------
+const { name, openingHours, categories } = restaurant;
+//console.log(name, openingHours, categories);
+//copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+//console.log(mainMenuCopy);
+
+// Merge array
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+//console.log(menu);
+//
+//CODING CHALLENGE 1
+//
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1
+//const [...players1] = game.players[0];
+//const [...players2] = game.players[1];
+//console.log(players1);
+//console.log(players2);
+const [players1, players2] = game.players;
+//console.log(players1, players2);
+
+// 2
+//const [gk, fieldPlayers] = [players1[0], ...players1];
+const [gk, ...fieldPlayers] = players1;
+//console.log(gk, fieldPlayers);
+// 3
+const allPlayers = [...players1, ...players2];
+//console.log(allPlayers);
+// 4
+const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+//console.log(playersFinal);
+// 5
+//const { team1, x, team2 } = game.odds;
+//console.log(team1, x, team2);
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+// 6
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+printGoals('Davies', 'Muller');
+
+// 7
+game.odds.team1 > game.odds.team2 && console.log('team1 more likely to win');
+game.odds.team1 < game.odds.team2 && console.log('team2 more likely to win');
