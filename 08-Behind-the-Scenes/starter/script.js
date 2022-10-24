438e92e1ca8d15c35e0965a02992203cfd31c24e
Alexandre Stanescot <alex@aerostan.com>
2022-10-24 15:26:55 +0200

    Excercices

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -5,7 +5,7 @@ function calcAge(birthYear) {
 
   function printAge() {
     const output = `You are ${age}, born in ${birthYear}`;
-    console.log(output);
+    //console.log(output);
   }
   printAge();
   return age;
@@ -13,3 +13,13 @@ function calcAge(birthYear) {
 
 const firstName = 'Alex';
 calcAge(1991);
+
+const alex = {
+  name: 'stan',
+  first: 'alex',
+};
+
+console.log(alex);
+
+const alex2 = Object.assign({}, alex);
+console.log(alex2);
